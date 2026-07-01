f19db3134f73f1c3542807ac220bdbc1253af9b6
kiratveersinghralhan-hue <kiratveersinghralhan@gmail.com>
2026-07-01 14:38:36 +0530

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,5 +0,0 @@
-/* Public browser configuration. The anon key is safe to expose when RLS is enabled. */
-window.VEYRATH_SUPABASE = Object.freeze({
-  url: 'https://rpsiddurmwtwvpnwzclo.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYXNlIiwicmVmIjoicnBzaWRkdXJtd3R3dnBudzJjbG8iLCJyb2xlIjoiYW5vbiIsImlhdCI6MTc3NTY4Nzk2MywiZXhwIjoyMDkxMjYzOTYzfQ.ROGegxjw-HOCkml-5vU19Fd5Bjwn64XpbGIagu8_DMc'
-});
